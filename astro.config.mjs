3489c513e820dfd5cd7a6e80eb032171ea1ccd7a
jmanzanares12 <jimgken2004@gmail.com>
2026-04-10 17:34:42 -0600

    Feat: added astro-icon and plugings and changed icons of services section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; //
+import tailwind from '@tailwindcss/vite'; 
+import icon from 'astro-icon';
 
 export default defineConfig({
+  integrations: [
+    icon(), 
+  ],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwind()],
   },
 });
